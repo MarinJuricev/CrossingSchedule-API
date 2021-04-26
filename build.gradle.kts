ed87786a35a2707c0eef8dc1015f6429731afe2d
MarinJ <mjuric00@fesb.hr>
2021-04-27 00:44:15 +0200

    Set the kotlin jvmTarget to 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,3 +83,9 @@ tasks.create("stage") {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
